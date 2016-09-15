e79b7e5686312479d22198d45f58c42271ed17a3
jb88229 <jacob.barel@gmail.com>
2016-09-15 16:58:32 +0300

    WIP: Updating main function.

--- src/com/mantilla/Main.java
+++ src/com/mantilla/Main.java
@@ -1,8 +1,28 @@
 package com.mantilla;
 
+import net.sourceforge.zmanim.*;
+import net.sourceforge.zmanim.util.*;
+import java.util.TimeZone;
+
 public class Main {
 
+
     public static void main(String[] args) {
-	    System.out.println("Please, indicate image path and options.");
+        String locationName = "Lakewood, NJ";
+        double latitude = 40.096; //latitude of Lakewood, NJ
+        double longitude = -74.222; //longitude of Lakewood, NJ
+        double elevation = 0; //optional elevation
+        //use a Valid Olson Database timezone listed in java.util.TimeZone.getAvailableIDs()
+        TimeZone timeZone = TimeZone.getTimeZone("America/New_York");
+        //create the location object
+        GeoLocation location = new GeoLocation(locationName, latitude, longitude, elevation, timeZone);
+        //create the ZmanimCalendar
+        ZmanimCalendar zc = new ZmanimCalendar(location);
+        //optionally set the internal calendar
+        //zc.getCalendar().set(1969, Calendar.FEBRUARY, 8);
+        System.out.println("Today's Zmanim for " + locationName);
+        System.out.println("Sunrise: " + zc.getSunrise()); //output sunrise
+        System.out.println("Sof Zman Shema GRA: " + zc.getSofZmanShmaGRA()); //output Sof Zman Shema GRA
+        System.out.println("Sunset: " + zc.getSunset()); //output sunset
     }
 }
